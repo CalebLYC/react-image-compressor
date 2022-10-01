302a40fea666e3aa37184ec3b179551631e543b3
CalebLYC <107321873+CalebLYC@users.noreply.github.com>
2022-10-01 01:18:03 +0000

    first compressor optimisation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
         <div className='App'>
             <h1>Compresser vos images</h1>
             <Compressor />
+            <p className='reference'>Fait par <a href="https://caleblyc.github.io/shopingCoin" target='_blank' rel='noreferrer'>Caleb Lyc</a></p>
         </div>
     )
 }
--- src/components/DownloadCard.jsx
+++ src/components/DownloadCard.jsx
@@ -4,7 +4,7 @@ function DownloadCard({ imagesrc, isCompressed, compressedImage }) {
     return (
         <div className='card'>
             <div>
-                <img src={`${imagesrc}`} alt="download" max-width='500' max-heigth='500' />
+                {!isCompressed ? <img src={`${imagesrc}`} alt="download" max-width='500' max-heigth='500' /> : <a href={imagesrc} rel='noreferrer' target='_blank'><img src={`${imagesrc}`} alt="download" max-width='500' max-heigth='500' /></a>}
             </div>
             {isCompressed && <a href={imagesrc} download={'compressed' + compressedImage.name}><button className='compressBtn'>Télécharger</button></a>}
         </div>
